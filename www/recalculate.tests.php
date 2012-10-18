b0d846b153999f736c9e54e34ccb9414f25af9a1
akucaniz <andre@ult.com.br>
2012-10-18 11:11:49 -0300

    SYSCLASS-274: Recalcular provas de Segurança da informação *IN PROGRESS*

--- www/recalculate.tests.php
+++ www/recalculate.tests.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Platform boleto page
- *
- * Esta página permite a visualização de um boleto através de um hash
- *
- * @package SysClass
- * @version 3.0.0
- */
-header("Location: index.php");
-exit;
 session_cache_limiter('nocache');
 session_start(); //This causes the double-login problem, where the user needs to login twice when already logged in with the same browser
 
@@ -17,9 +7,11 @@ $path = "../libraries/";
 /** Configuration file */
 require_once $path."configuration.php";
 
+header("content-type: text/plain");
+
 /* BUSCAR TESTS BY UNIT */
 /* COMO RECALCULAR */
-$TestID		= 365;
+$TestID		= 436;
 //$QuestionID = 1533;
 
 $test = new MagesterTest($TestID);
@@ -32,7 +24,7 @@ foreach($completedTests as $testData) {
 	
 	$logData = array(
 		'ID_TESTE' => $testData['tests_ID'],
-		'NOME'		=> "Engenharia de software – Gerência de projetos II - Prova Online",
+		'NOME'		=> "Segurança da Informação - Gestão de Segurança - Prova on line",
 		'ID' => $testData['id'],
 		'LOGIN' => $testData['users_LOGIN'],
 		'OLD_SCORE' => $showTest -> completedTest['score'],
@@ -42,7 +34,7 @@ foreach($completedTests as $testData) {
 
 	foreach($showTest->questions as $QuestionID => $question) {
 		//$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
-		
+		/*
 		if (!array_key_exists($QuestionID, $testQuestions)) {
 		    // QUESTION REMOVED FROM TEST
 		    unset($showTest->questions[$QuestionID]);
@@ -55,9 +47,12 @@ foreach($completedTests as $testData) {
 				$showTest->questions[$QuestionID]->options = unserialize($testQuestions[$QuestionID]['options']);
 			}
 		}
-		
-		if ($QuestionID == 6184) {
-			$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
+		*/
+		if ($QuestionID == 7830) {
+			if ($showTest->questions[$QuestionID]->userAnswer == 4) {
+				$showTest->questions[$QuestionID]->userAnswer = 2;
+				echo "<br />";
+			}
 		}
 		
 		$results = $showTest->questions[$QuestionID]->correct();
@@ -75,18 +70,16 @@ foreach($completedTests as $testData) {
 		$logData['NEW_SCORE'] = $showTest -> completedTest['score'];
 		$diff[] = $logData;
 	}
-	
-	//var_dump($logData);
-	
 	echo "\n\n";
 }
 
-/*
-$TestID		= 198;
-//$QuestionID = 1533;
+
+
+/* BUSCAR TESTS BY UNIT */
+/* COMO RECALCULAR */
+$TestID		= 208;
 
 $test = new MagesterTest($TestID);
-
 $testQuestions = $test->getQuestions();
 
 $completedTests = eF_getTableData("completed_tests", "*", "tests_ID=" . $TestID);
@@ -96,7 +89,7 @@ foreach($completedTests as $testData) {
 
 	$logData = array(
 			'ID_TESTE' => $testData['tests_ID'],
-			'NOME'		=> "Engenharia de software – Gerência de projetos II - Prova Online",
+			'NOME'		=> "Segurança da Informação - Gestão de Segurança - Prova Presencial",
 			'ID' => $testData['id'],
 			'LOGIN' => $testData['users_LOGIN'],
 			'OLD_SCORE' => $showTest -> completedTest['score'],
@@ -105,12 +98,69 @@ foreach($completedTests as $testData) {
 	$showTest -> completedTest['score'] = 0;
 
 	foreach($showTest->questions as $QuestionID => $question) {
-		//$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
+		
+		if ($QuestionID == 7869) {
+			$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
+		}
+		
+		if ($QuestionID == 7861) {
+			$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
+		}
+		
+		if ($QuestionID == 7855) {
+			$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);
+		}
+		
+		$results = $showTest->questions[$QuestionID]->correct();
+		$showTest->questions[$QuestionID] -> score = round($results['score'] * 100, 2);
 
-		if (!array_key_exists($QuestionID, $testQuestions)) {
-			// QUESTION REMOVED FROM TEST
-			unset($showTest->questions[$QuestionID]);
-			continue;
+		$showTest->questions[$QuestionID] -> results = $results['correct'];
+		$showTest -> completedTest['score'] += $showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID); //the total test score
+		$showTest->questions[$QuestionID] -> scoreInTest = round($showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID), 3); //Score in test is the question score, weighted with the question's weight in the test
+
+	}
+
+	if ($showTest -> completedTest['score'] != $logData['OLD_SCORE']) {
+		$showTest->save();
+
+		$logData['NEW_SCORE'] = $showTest -> completedTest['score'];
+		$diff[] = $logData;
+	}
+	echo "\n\n";
+}
+
+
+
+/* BUSCAR TESTS BY UNIT */
+/* COMO RECALCULAR */
+$TestID		= 207;
+//$QuestionID = 1533;
+
+$test = new MagesterTest($TestID);
+$testQuestions = $test->getQuestions();
+
+$completedTests = eF_getTableData("completed_tests", "*", "tests_ID=" . $TestID);
+
+foreach($completedTests as $testData) {
+	$showTest = unserialize($testData['test']);
+
+	$logData = array(
+			'ID_TESTE' => $testData['tests_ID'],
+			'NOME'		=> "Segurança da Informação - Introdução Forense - Prova Presencial",
+			'ID' => $testData['id'],
+			'LOGIN' => $testData['users_LOGIN'],
+			'OLD_SCORE' => $showTest -> completedTest['score'],
+	);
+
+	$showTest -> completedTest['score'] = 0;
+
+	foreach($showTest->questions as $QuestionID => $question) {
+		if ($QuestionID == 8880) {
+			if ($showTest->questions[$QuestionID]->answer[0] != $showTest->questions[$QuestionID]->userAnswer) {
+				$showTest->questions[$QuestionID]->userAnswer = $showTest->questions[$QuestionID]->answer[0];
+				
+			}
+			echo "\n";
 		}
 
 		$results = $showTest->questions[$QuestionID]->correct();
@@ -128,108 +178,9 @@ foreach($completedTests as $testData) {
 		$logData['NEW_SCORE'] = $showTest -> completedTest['score'];
 		$diff[] = $logData;
 	}
+	echo "\n\n";
 }
 
-*/
-
-/*
-$TestID     = 154;
-//$QuestionID = 1533;
-
-$test = new MagesterTest($TestID);
-
-$testQuestions = $test->getQuestions();
-$completedTests = eF_getTableData("completed_tests", "*", "tests_ID=" . $TestID);
-
-foreach($completedTests as $testData) {
-    $showTest = unserialize($testData['test']);
-    
-    $logData = array(
-        'ID_TESTE' => $testData['tests_ID'],
-        'NOME'      => "",
-        'ID' => $testData['id'],
-        'LOGIN' => $testData['users_LOGIN'],
-        'OLD_SCORE' => $showTest -> completedTest['score'],
-    );
-    
-    $showTest -> completedTest['score'] = 0;
-
-    foreach($showTest->questions as $QuestionID => $question) {
-        //$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']); 
-        if (!array_key_exists($QuestionID, $testQuestions)) {
-            // QUESTION REMOVED FROM TEST
-            unset($showTest->questions[$QuestionID]);
-            continue;
-        }
-
-        $results = $showTest->questions[$QuestionID]->correct();
-        
-        $showTest->questions[$QuestionID] -> score = round($results['score'] * 100, 2);
-        
-        $showTest->questions[$QuestionID] -> results = $results['correct'];
-        $showTest -> completedTest['score'] += $showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID); //the total test score
-        $showTest->questions[$QuestionID] -> scoreInTest = round($showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID), 3); //Score in test is the question score, weighted with the question's weight in the test
-        
-    }
-    
-
-    if ($showTest -> completedTest['score'] != $logData['OLD_SCORE']) {
-        $showTest->save();
-        
-        $logData['NEW_SCORE'] = $showTest -> completedTest['score'];
-        $diff[] = $logData;
-    }
-}
-*/
-
-
-
-
-/*
-$TestID		= 70;
-$QuestionID = 664;
-
-$test = new MagesterTest($TestID);
-
-$testQuestions = $test->getQuestions();
-
-$completedTests = eF_getTableData("completed_tests", "*", "tests_ID=" . $TestID);
-
-foreach($completedTests as $testData) {
-	$showTest = unserialize($testData['test']);
-	
-	$logData = array(
-		'ID_TESTE' => $testData['tests_ID'],
-		'NOME'		=> "Bioenergia – Geração e Caracterização de Matéria-Prima I – Prova online",
-		'ID' => $testData['id'],
-		'LOGIN' => $testData['users_LOGIN'],
-		'OLD_SCORE' => $showTest -> completedTest['score'],
-	);
-	
-	$showTest -> completedTest['score'] = 0;
-	
-	foreach($showTest->questions as $QuestionID => $question) {
-		$showTest->questions[$QuestionID]->answer = unserialize($testQuestions[$QuestionID]['answer']);	
-		
-		$results = $showTest->questions[$QuestionID]->correct();
-		$showTest->questions[$QuestionID] -> score = round($results['score'] * 100, 2);
-		
-		$showTest->questions[$QuestionID] -> results = $results['correct'];
-		$showTest -> completedTest['score'] += $showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID); //the total test score
-		$showTest->questions[$QuestionID] -> scoreInTest = round($showTest->questions[$QuestionID] -> score * $showTest -> getQuestionWeight($QuestionID), 3); //Score in test is the question score, weighted with the question's weight in the test
-		
-	}
-	
-
-	if ($showTest -> completedTest['score'] != $logData['OLD_SCORE']) {
-		//$showTest->save();
-		
-		$logData['NEW_SCORE'] = $showTest -> completedTest['score'];
-		$diff[] = $logData;
-	}
-}
-
-*/
 echo implode(";", array_keys($diff[0]));
 echo "\n";
 foreach($diff as $item) {
