4741bd8b30a3767418594c885318cfae30107b50
sysclass.com Robot <andre@ult.com.br>
2013-04-08 14:03:29 -0300

    Correção nas funções que geram o código de barras dos boletos

--- www/modules/module_xpay_boleto/functions/smarty/itau_fati.xpay_boleto_FBarCode.php
+++ www/modules/module_xpay_boleto/functions/smarty/itau_fati.xpay_boleto_FBarCode.php
@@ -3,5 +3,5 @@ require_once(dirname(__FILE__) . "/super.itau.xpay_boleto_FBarCode.php");
 
 function xpay_boleto_itau_fati_FBarCode($params, &$smarty)
 {
-	return xpay_boleto_super_itau_FBarCode($params, &$smarty);
+	return xpay_boleto_super_itau_FBarCode($params, $smarty);
 } //Fim da função
