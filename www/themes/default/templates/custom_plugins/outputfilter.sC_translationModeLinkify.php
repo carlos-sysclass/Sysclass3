68eeb11f03ad05317f342674065b67b652abccba
akucaniz <andre@plico.com.br>
2013-08-02 00:12:22 -0300

    Atualização do módulo de traduçõa para contemplar outros tipos de usuários

--- www/themes/default/templates/custom_plugins/outputfilter.sC_translationModeLinkify.php
+++ www/themes/default/templates/custom_plugins/outputfilter.sC_translationModeLinkify.php
@@ -7,23 +7,52 @@ function smarty_outputfilter_sC_translationModeLinkify($compiled, &$smarty)
 {
 	global $currentUser;
 	$currentLanguage = $smarty->get_template_vars("T_BASE_LANGUAGE");
-	if ($_SESSION['translation_mode'] && !is_null($currentUser) && $currentUser->user['user_type'] == 'administrator') {
+
+	$userGroups = $currentUser->getGroups();
+	$userGroupsIDs = array_keys($userGroups);
+	$translationModeGroup = 12;
+
+	if ($_SESSION['translation_mode'] && !is_null($currentUser) && in_array($translationModeGroup, $userGroupsIDs)) {
 	
 		$modules = sC_loadAllModules(true);
 		$languages = $modules['module_language']->getDisponibleLanguages();
 
-//		T_LANGUAGE_LANGUAGES
+		$allTokens = $modules['module_language']->parseTokensFromSource();
+
+
+		foreach($allTokens as $key => $value) {
+			if (!strpos($compiled, $key)) {
+				unset($allTokens[$key]);
+			}
+		}
+		$langTokens = array();
 		foreach($languages as $language) {
-			$tokens = $modules['module_language']->getTranslatedTokensAction($language);
-			$tokens = $tokens['terms'];
-			// CHECK TOKENS ON PAGE
-			foreach($tokens as $key => $value) {
+			$langTokens[$language] = $modules['module_language']->getTranslatedTokensAction($language);
+			$langTokens[$language] = $langTokens[$language]['terms'];
+			foreach($langTokens[$language] as $key => $value) {
 				if (!strpos($compiled, $key)) {
-					unset($tokens[$key]);
+					unset($langTokens[$language][$key]);
 				} else {
-					//$compiled = str_replace($key, "@" . $key . "@", $compiled);
+					// IF THIS TOKEN DOES NOT EXISTS ON allTokens, INSERT THEN
+					if (!array_key_exists($key, $allTokens)) {
+						$allTokens[$key] = $key;
+					}
 				}
 			}
+		}
+
+//		T_LANGUAGE_LANGUAGES
+		foreach($languages as $language) {
+			$tokens = array();
+			// CHECK TOKENS ON PAGE
+			foreach($allTokens as $key => $value) {
+				if (array_key_exists($key, $langTokens[$language])) {
+					$tokens[$key] = $langTokens[$language][$key];
+				} else {
+					$tokens[$key] = $allTokens[$key];
+				}
+			}
+
 			$modules['module_language']->setUsedPageTerms($language, $tokens);
 			
 			$smarty->assign("T_" . strtoupper($language) . "_USED_TERMS", $tokens);
@@ -31,7 +60,6 @@ function smarty_outputfilter_sC_translationModeLinkify($compiled, &$smarty)
 				$smarty->assign("T_CURRENT_USED_TERMS", $tokens);
 				$modules['module_language']->setUsedDefaultLanguage($language);
 			}
-
 		}
 	}
     return $compiled;
