49320459b85fac37a18795cc7e53eaf976de96df
akucaniz-home <andre@plico.com.br>
2013-11-14 17:21:17 -0200

    Adicionada configuração para layout.sysclass

--- libraries/configuration.php
+++ libraries/configuration.php
@@ -94,39 +94,46 @@ $configurationDefaults = array(
 		'dbname'	=> 'sysclass_idiompro',
 		'overrideTheme' => 'sysclass3'
 	),
-		'demo1.sysclass.com'	=> array(
-				'dbname'	=> 'sysclass_demo1',
-				'overrideTheme' => 'sysclass3'
-		),
-		'www.demo1.sysclass.com'     => array(
-				'dbname'        => 'sysclass_demo1',
-				'overrideTheme' => 'sysclass3'
-		),
-		'demo2.sysclass.com'	=> array(
-				'dbname'	=> 'sysclass_demo2',
-				'overrideTheme' => 'sysclass3'
-		),
-		'www.demo2.sysclass.com'     => array(
-				'dbname'        => 'sysclass_demo2',
-				'overrideTheme' => 'sysclass3'
-		),
-		'demo3.sysclass.com'	=> array(
-				'dbname'	=> 'sysclass_demo3',
-				'overrideTheme' => 'sysclass3'
-		),
-		'www.demo3.sysclass.com'     => array(
-				'dbname'        => 'sysclass_demo3',
-				'overrideTheme' => 'sysclass3'
-		),
-		'demo4.sysclass.com'	=> array(
-				'dbname'	=> 'sysclass_demo4',
-				'overrideTheme' => 'sysclass3'
-		),
-		'www.demo4.sysclass.com'     => array(
-				'dbname'        => 'sysclass_demo4',
-				'overrideTheme' => 'sysclass3'
-		),
-		
+	'demo1.sysclass.com'	=> array(
+			'dbname'	=> 'sysclass_demo1',
+			'overrideTheme' => 'sysclass3'
+	),
+	'www.demo1.sysclass.com'     => array(
+			'dbname'        => 'sysclass_demo1',
+			'overrideTheme' => 'sysclass3'
+	),
+	'demo2.sysclass.com'	=> array(
+			'dbname'	=> 'sysclass_demo2',
+			'overrideTheme' => 'sysclass3'
+	),
+	'www.demo2.sysclass.com'     => array(
+			'dbname'        => 'sysclass_demo2',
+			'overrideTheme' => 'sysclass3'
+	),
+	'demo3.sysclass.com'	=> array(
+			'dbname'	=> 'sysclass_demo3',
+			'overrideTheme' => 'sysclass3'
+	),
+	'www.demo3.sysclass.com'     => array(
+			'dbname'        => 'sysclass_demo3',
+			'overrideTheme' => 'sysclass3'
+	),
+	'demo4.sysclass.com'	=> array(
+			'dbname'	=> 'sysclass_demo4',
+			'overrideTheme' => 'sysclass3'
+	),
+	'www.demo4.sysclass.com'     => array(
+			'dbname'        => 'sysclass_demo4',
+			'overrideTheme' => 'sysclass3'
+	),
+	'layout.sysclass.com'	=> array(
+		'dbname'        => 'sysclass_layout',
+		'overrideTheme' => 'sysclass3'
+	),
+	'www.layout.sysclass.com'	=> array(
+        'dbname'        => 'sysclass_layout',
+        'overrideTheme' => 'sysclass3'
+	)
 );
 
 $configuration = array_merge($configurationDefaults['_default'], $configurationDefaults[$_SERVER["SERVER_NAME"]]);
