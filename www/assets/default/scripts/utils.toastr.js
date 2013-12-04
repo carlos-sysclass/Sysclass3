0116534e338cfa6299b6720d52c833e284544fd4
akucaniz-home <andre@plico.com.br>
2013-12-04 16:42:46 -0200

    Corrigindo problema com requisição falhas

--- www/assets/default/scripts/utils.toastr.js
+++ www/assets/default/scripts/utils.toastr.js
@@ -1,23 +1,25 @@
 $SC.module("utils.toastr", function(mod, app, Backbone, Marionette, $, _){
 
 	mod.addInitializer(function(){
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "positionClass": "toast-top-center",
-            "showDuration": "1000",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        };
+        if (typeof toastr != 'undefined') {
 
-        this.message = function(type, message) {
-            console.log(type);
-            return toastr[type](message);
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "positionClass": "toast-top-center",
+                "showDuration": "1000",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+            this.message = function(type, message) {
+                console.log(type);
+                return toastr[type](message);
+            }
         }
   	});
 
