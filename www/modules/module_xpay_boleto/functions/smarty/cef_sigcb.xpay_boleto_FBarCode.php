8c2d6be87f93914ab4cc5ea94bc8bf7ee30d420d
André Kucaniz <andre@kucaniz.com>
2014-02-18 15:32:04 -0300

    Ajustando chamadas de funcções do boleto

--- www/modules/module_xpay_boleto/functions/smarty/cef_sigcb.xpay_boleto_FBarCode.php
+++ www/modules/module_xpay_boleto/functions/smarty/cef_sigcb.xpay_boleto_FBarCode.php
@@ -1,93 +1,98 @@
 <?php
-function esquerda($entra,$comp)
-{
-	return substr($entra,0,$comp);
+if (!function_exists('esquerda')) { 
+	function esquerda($entra,$comp)
+	{
+		return substr($entra,0,$comp);
+	}
 }
-
-function direita($entra,$comp)
-{
-	return substr($entra,strlen($entra)-$comp,$comp);
+if (!function_exists('direita')) { 
+	function direita($entra,$comp)
+	{
+		return substr($entra,strlen($entra)-$comp,$comp);
+	}
 }
-function xpay_boleto_cef_sigcb_FBarCode($params, &$smarty)
-{
-	$valor = $params['barcode'];
-	$baseModuleLink	= $params['module_link'];
+if (!function_exists('xpay_boleto_cef_sigcb_FBarCode')) { 
+	function xpay_boleto_cef_sigcb_FBarCode($params, &$smarty)
+	{
+		$valor = $params['barcode'];
+		$baseModuleLink	= $params['module_link'];
 
-	$fino = 1 ;
-	$largo = 3 ;
-	$altura = 50 ;
+		$fino = 1 ;
+		$largo = 3 ;
+		$altura = 50 ;
 
-	$barcodes[0] = "00110" ;
-	$barcodes[1] = "10001" ;
-	$barcodes[2] = "01001" ;
-	$barcodes[3] = "11000" ;
-	$barcodes[4] = "00101" ;
-	$barcodes[5] = "10100" ;
-	$barcodes[6] = "01100" ;
-	$barcodes[7] = "00011" ;
-	$barcodes[8] = "10010" ;
-	$barcodes[9] = "01010" ;
-	for ($f1=9;$f1>=0;$f1--) {
-		for ($f2=9;$f2>=0;$f2--) {
-			$f = ($f1 * 10) + $f2 ;
-			$texto = "" ;
-			for ($i=1;$i<6;$i++) {
-				$texto .=  substr($barcodes[$f1],($i-1),1) . substr($barcodes[$f2],($i-1),1);
+		$barcodes[0] = "00110" ;
+		$barcodes[1] = "10001" ;
+		$barcodes[2] = "01001" ;
+		$barcodes[3] = "11000" ;
+		$barcodes[4] = "00101" ;
+		$barcodes[5] = "10100" ;
+		$barcodes[6] = "01100" ;
+		$barcodes[7] = "00011" ;
+		$barcodes[8] = "10010" ;
+		$barcodes[9] = "01010" ;
+		for ($f1=9;$f1>=0;$f1--) {
+			for ($f2=9;$f2>=0;$f2--) {
+				$f = ($f1 * 10) + $f2 ;
+				$texto = "" ;
+				for ($i=1;$i<6;$i++) {
+					$texto .=  substr($barcodes[$f1],($i-1),1) . substr($barcodes[$f2],($i-1),1);
+				}
+				$barcodes[$f] = $texto;
 			}
-			$barcodes[$f] = $texto;
 		}
-	}
-	//Desenho da barra
+		//Desenho da barra
 
-	//Guarda inicial
-	$html = sprintf('
-		<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-		<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
-		<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-		<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
-		', $fino, $altura, $baseModuleLink
-	);
+		//Guarda inicial
+		$html = sprintf('
+			<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+			<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
+			<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+			<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
+			', $fino, $altura, $baseModuleLink
+		);
 
-	$texto = $valor ;
-	if ((strlen($texto) % 2) <> 0) {
-		$texto = "0" . $texto;
-	}
+		$texto = $valor ;
+		if ((strlen($texto) % 2) <> 0) {
+			$texto = "0" . $texto;
+		}
 
-	// Draw dos dados
-	while (strlen($texto) > 0) {
-	  $i = round(esquerda($texto,2));
-	  $texto = direita($texto,strlen($texto)-2);
-	  $f = $barcodes[$i];
-	  for ($i=1;$i<11;$i+=2) {
-	    if (substr($f,($i-1),1) == "0") {
-	      $f1 = $fino ;
-	    } else {
-	      $f1 = $largo ;
-	    }
-	    $html .= sprintf(
-	    	'<img src="%3$simages/layouts/p.png" width="%1$d" height="%2$d" border="0">',
-	    	$f1, $altura, $baseModuleLink
-	   	);
+		// Draw dos dados
+		while (strlen($texto) > 0) {
+		  $i = round(esquerda($texto,2));
+		  $texto = direita($texto,strlen($texto)-2);
+		  $f = $barcodes[$i];
+		  for ($i=1;$i<11;$i+=2) {
+		    if (substr($f,($i-1),1) == "0") {
+		      $f1 = $fino ;
+		    } else {
+		      $f1 = $largo ;
+		    }
+		    $html .= sprintf(
+		    	'<img src="%3$simages/layouts/p.png" width="%1$d" height="%2$d" border="0">',
+		    	$f1, $altura, $baseModuleLink
+		   	);
 
-	    if (substr($f,$i,1) == "0") {
-	      $f2 = $fino ;
-	    } else {
-	      $f2 = $largo ;
-	    }
-	    $html .= sprintf(
-	   		'<img src="%3$simages/layouts/b.png" width="%1$d" height="%2$d" border="0">',
-	   		$f2, $altura, $baseModuleLink
-	    );
-	  }
-	}
+		    if (substr($f,$i,1) == "0") {
+		      $f2 = $fino ;
+		    } else {
+		      $f2 = $largo ;
+		    }
+		    $html .= sprintf(
+		   		'<img src="%3$simages/layouts/b.png" width="%1$d" height="%2$d" border="0">',
+		   		$f2, $altura, $baseModuleLink
+		    );
+		  }
+		}
 
-// 	Draw guarda final
-	//Guarda inicial
-	$html .= sprintf('
-		<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-		<img src=%4$simages/layouts/b.png width="%3$d" height="%2$d" border=0>
-		<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-		', $largo, $altura, $fino, $baseModuleLink
-	);
-	return $html;
-} //Fim da função
+	// 	Draw guarda final
+		//Guarda inicial
+		$html .= sprintf('
+			<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+			<img src=%4$simages/layouts/b.png width="%3$d" height="%2$d" border=0>
+			<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+			', $largo, $altura, $fino, $baseModuleLink
+		);
+		return $html;
+	} //Fim da função
+}
--- www/modules/module_xpay_boleto/functions/smarty/super.itau.xpay_boleto_FBarCode.php
+++ www/modules/module_xpay_boleto/functions/smarty/super.itau.xpay_boleto_FBarCode.php
@@ -11,94 +11,95 @@ if (!function_exists('direita')) {
 		return substr($entra,strlen($entra)-$comp,$comp);
 	}
 }
+if (!function_exists('xpay_boleto_super_itau_FBarCode')) {
+	function xpay_boleto_super_itau_FBarCode($params, &$smarty)
+	{
+		$valor = $params['barcode'];
+		$baseModuleLink	= $params['module_link'];
 
-function xpay_boleto_super_itau_FBarCode($params, &$smarty)
-{
-	$valor = $params['barcode'];
-	$baseModuleLink	= $params['module_link'];
+		$fino = 1 ;
+		$largo = 3 ;
+		$altura = 50 ;
 
-	$fino = 1 ;
-	$largo = 3 ;
-	$altura = 50 ;
-
-	$barcodes[0] = "00110" ;
-	$barcodes[1] = "10001" ;
-	$barcodes[2] = "01001" ;
-	$barcodes[3] = "11000" ;
-	$barcodes[4] = "00101" ;
-	$barcodes[5] = "10100" ;
-	$barcodes[6] = "01100" ;
-	$barcodes[7] = "00011" ;
-	$barcodes[8] = "10010" ;
-	$barcodes[9] = "01010" ;
-	for ($f1=9;$f1>=0;$f1--) {
-		for ($f2=9;$f2>=0;$f2--) {
-			$f = ($f1 * 10) + $f2 ;
-			$texto = "" ;
-			for ($i=1;$i<6;$i++) {
-				$texto .=  substr($barcodes[$f1],($i-1),1) . substr($barcodes[$f2],($i-1),1);
+		$barcodes[0] = "00110" ;
+		$barcodes[1] = "10001" ;
+		$barcodes[2] = "01001" ;
+		$barcodes[3] = "11000" ;
+		$barcodes[4] = "00101" ;
+		$barcodes[5] = "10100" ;
+		$barcodes[6] = "01100" ;
+		$barcodes[7] = "00011" ;
+		$barcodes[8] = "10010" ;
+		$barcodes[9] = "01010" ;
+		for ($f1=9;$f1>=0;$f1--) {
+			for ($f2=9;$f2>=0;$f2--) {
+				$f = ($f1 * 10) + $f2 ;
+				$texto = "" ;
+				for ($i=1;$i<6;$i++) {
+					$texto .=  substr($barcodes[$f1],($i-1),1) . substr($barcodes[$f2],($i-1),1);
+				}
+				$barcodes[$f] = $texto;
 			}
-			$barcodes[$f] = $texto;
 		}
-	}
 
-	$valor = $params['barcode'];
-	$baseModuleLink	= $params['module_link'];
+		$valor = $params['barcode'];
+		$baseModuleLink	= $params['module_link'];
 
-	//Desenho da barra
-	//Guarda inicial
-	$html = sprintf('
-			<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-			<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
-			<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-			<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
-			', $fino, $altura, $baseModuleLink
-	);
+		//Desenho da barra
+		//Guarda inicial
+		$html = sprintf('
+				<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+				<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
+				<img src=%3$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+				<img src=%3$simages/layouts/b.png width="%1$d" height="%2$d" border=0>
+				', $fino, $altura, $baseModuleLink
+		);
 
-	//Guarda inicial
-	$texto = $valor ;
-	if ((strlen($texto) % 2) <> 0) {
-		$texto = "0" . $texto;
-	}
-
-	// Draw dos dados
-	while (strlen($texto) > 0) {
-		$i = round(esquerda($texto,2));
-		$texto = direita($texto,strlen($texto)-2);
-		$f = $barcodes[$i];
-		for ($i=1;$i<11;$i+=2) {
-			if (substr($f,($i-1),1) == "0") {
-				$f1 = $fino ;
-			} else {
-				$f1 = $largo ;
-			}
-
-			$html .= sprintf(
-					'<img src="%3$simages/layouts/p.png" width="%1$d" height="%2$d" border="0">',
-					$f1, $altura, $baseModuleLink
-			);
-
-			if (substr($f,$i,1) == "0") {
-				$f2 = $fino ;
-			} else {
-				$f2 = $largo ;
-			}
-
-			$html .= sprintf(
-					'<img src="%3$simages/layouts/b.png" width="%1$d" height="%2$d" border="0">',
-					$f2, $altura, $baseModuleLink
-			);
-
+		//Guarda inicial
+		$texto = $valor ;
+		if ((strlen($texto) % 2) <> 0) {
+			$texto = "0" . $texto;
 		}
-	}
 
-	// Draw guarda final
-	$html .= sprintf('
-			<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-			<img src=%4$simages/layouts/b.png width="%3$d" height="%2$d" border=0>
-			<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
-			', $largo, $altura, $fino, $baseModuleLink
-	);
-	return $html;
+		// Draw dos dados
+		while (strlen($texto) > 0) {
+			$i = round(esquerda($texto,2));
+			$texto = direita($texto,strlen($texto)-2);
+			$f = $barcodes[$i];
+			for ($i=1;$i<11;$i+=2) {
+				if (substr($f,($i-1),1) == "0") {
+					$f1 = $fino ;
+				} else {
+					$f1 = $largo ;
+				}
 
-} //Fim da função
+				$html .= sprintf(
+						'<img src="%3$simages/layouts/p.png" width="%1$d" height="%2$d" border="0">',
+						$f1, $altura, $baseModuleLink
+				);
+
+				if (substr($f,$i,1) == "0") {
+					$f2 = $fino ;
+				} else {
+					$f2 = $largo ;
+				}
+
+				$html .= sprintf(
+						'<img src="%3$simages/layouts/b.png" width="%1$d" height="%2$d" border="0">',
+						$f2, $altura, $baseModuleLink
+				);
+
+			}
+		}
+
+		// Draw guarda final
+		$html .= sprintf('
+				<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+				<img src=%4$simages/layouts/b.png width="%3$d" height="%2$d" border=0>
+				<img src=%4$simages/layouts/p.png width="%1$d" height="%2$d" border=0>
+				', $largo, $altura, $fino, $baseModuleLink
+		);
+		return $html;
+
+	} //Fim da função
+}
