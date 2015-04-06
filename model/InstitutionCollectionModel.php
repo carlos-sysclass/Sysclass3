f87dab77d1b0ec6c410b878979cc487707dcfd02
lucas.eduardo <lucas.eduardo@beeleads.com.br>
2015-04-06 16:13:15 -0300

    Exibindo website, addres e facebook da instituição na página do aluno.

--- model/InstitutionCollectionModel.php
+++ model/InstitutionCollectionModel.php
@@ -12,4 +12,44 @@ class InstitutionCollectionModel extends AbstractSysclassModel implements ISyncr
  		parent::init();
 
 	}
+
+	public function getInstitution($id)
+    {
+    	if($id == 0)
+    	{
+    		$sql = sprintf(
+	           "SELECT 
+				  inst.address,
+				  inst.active,
+				  inst.website,
+				  inst.facebook
+				FROM
+				  mod_institution inst
+				WHERE
+				  inst.id = 1"
+       		);
+    	}
+    	else
+    	{
+			$sql = sprintf(
+				"SELECT 
+					inst.address,
+					inst.active,
+					inst.website,
+					inst.facebook
+				FROM
+					mod_institution inst
+				LEFT JOIN
+					mod_classes clas
+				ON
+					(inst.id = clas.ies_id)
+				LEFT JOIN
+					users_to_classes ucla
+				ON
+					(ucla.classes_ID = clas.id) AND (ucla.users_ID = " . $id .")"
+			);
+   		}
+
+       return $this->db->GetArray($sql);
+    }
 }
