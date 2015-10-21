97641667f591c591e36a8d1844152e0ac5d8d9dc
André Kucaniz <andre@kucaniz.com>
2015-10-21 14:51:01 -0200

    SC-220 #resolved SC-220 #comment Incluing a option to aprove the pending users on user view table

--- www/assets/default/scripts/utils.datatables.js
+++ www/assets/default/scripts/utils.datatables.js
@@ -54,6 +54,24 @@ $SC.module("utils.datatables", function(mod, app, Backbone, Marionette, $, _) {
 				}
 				*/
         	},
+			doAction : function(e) {
+				var item = $(e.currentTarget);
+				if (item.data("actionUrl")) {
+					var url = item.data("actionUrl");
+					var method = "GET";
+					if (item.data("actionMethod")) {
+						method = item.data("actionMethod");
+					}
+
+	                $.ajax(url, { method : method } );
+				} else {
+					var data = this.oTable._($(e.currentTarget).closest("tr"));
+					this.trigger("action.datatables", $(e.currentTarget).closest("tr").get(0), _.first(data), $(e.currentTarget).data("datatableAction"));
+
+					//var data = this.oTable._($(e.currentTarget).closest("tr"));
+					//mod.trigger("datatable:item:click", item, _.first(data));
+				}
+			},
 
         	checkItem: function(e) {
 				e.preventDefault();
@@ -148,21 +166,7 @@ $SC.module("utils.datatables", function(mod, app, Backbone, Marionette, $, _) {
 				});
 			},
 			*/
-			doAction : function(e) {
-				var item = $(e.currentTarget);
-				if (item.data("actionUrl")) {
-					var url = item.data("actionUrl");
-					var method = "GET";
-					if (item.data("actionMethod")) {
-						method = item.data("actionMethod");
-					}
 
-	                $.ajax(url, { method : method } );
-				} else {
-					var data = this.oTable._($(e.currentTarget).closest("tr"));
-					mod.trigger("datatable:item:click", item, _.first(data));
-				}
-			}
         });
 	});
 });
