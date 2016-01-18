97804a0699d9bd152ceca13d3df0500e81452815
walter clak <walterelias10@gmail.com>
2016-01-18 16:01:25 -0200

    SC-329 #resolved SC-329 #comment Criação de esqueleto de módulo de pagamentos

---
+++ app/models/Payments/PaymentItem.php
@@ -0,0 +1,64 @@
+<?php
+namespace Sysclass\Models\Payments;
+
+use Plico\Mvc\Model,
+    Sysclass\Models\Acl\Resource,
+    Sysclass\Models\Acl\RolesUsers;
+
+class PaymentItem extends Model
+{
+    public function initialize()
+    {
+        $this->setSource("mod_payment_itens");
+
+        $this->belongsTo("payment_id", "Sysclass\\Models\\Payments\\Payment", "id",  array('alias' => 'payment'));
+
+         //$this->skipAttributesOnCreate(array('active'));
+
+        //$this->belongsTo("group_id", "Sysclass\\Models\\Users\\Group", "id",  array('alias' => 'group'));
+        /*
+        $this->belongsTo("language_id", "Sysclass\\Models\\I18n\\Language", "id",  array('alias' => 'language'));
+
+        $this->hasOne("id", "Sysclass\\Models\\Users\\UserAvatar", "user_id",  array('alias' => 'avatar'));
+
+        $this->hasMany("id", "Sysclass\\Models\\Users\\Settings", "user_id",  array('alias' => 'settings'));
+
+        $this->hasManyToMany(
+            "id",
+            "Sysclass\\Models\\Enrollments\\Course",
+            "user_id", "course_id",
+            "Sysclass\\Models\\Courses\\Course",
+            "id",
+            array('alias' => 'Courses')
+        );
+
+        $this->hasManyToMany(
+            "id",
+            "Sysclass\\Models\\Users\\UserAvatar",
+            "user_id", "file_id",
+            "Sysclass\\Models\\Dropbox\\File",
+            "id",
+            array('alias' => 'Avatars', 'reusable' => true)
+        );
+
+        $this->hasManyToMany(
+            "id",
+            "Sysclass\Models\Acl\RolesUsers",
+            "user_id", "role_id",
+            "Sysclass\Models\Acl\Role",
+            "id",
+            array('alias' => 'UserRoles', 'reusable' => true)
+        );
+
+        $this->hasManyToMany(
+            "id",
+            "Sysclass\Models\Users\UsersGroups",
+            "user_id", "group_id",
+            "Sysclass\Models\Users\Group",
+            "id",
+            array('alias' => 'UserGroups')
+        );
+        */
+
+    }
+}
