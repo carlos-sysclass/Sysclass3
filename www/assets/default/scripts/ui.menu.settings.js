8bd8e589e58e248cc5bd9934aead020de5f54605
André Kucaniz <andre@kucaniz.com>
2016-03-10 14:58:20 -0300

    SC-389 #resolved SC-389 #comment Correcting some js bugs in menu section script and putting up the correct script

--- www/assets/default/scripts/ui.menu.settings.js
+++ www/assets/default/scripts/ui.menu.settings.js
@@ -2,10 +2,10 @@ $SC.module("menu.settings", function(mod, app, Backbone, Marionette, $, _) {
 
 	mod.on("start", function() {
         // pageguide init
-        if (!_.usUndefined(tl)) {
+        if (!_.isUndefined(tl)) {
 	        tl.pg.init({
 	            /* pg_caption : "" */
-	            custom_open_button : "#open-pageguide-action"
+	            custom_open_button : "#open-pageguide-action a"
 	        });
         }
 	});
