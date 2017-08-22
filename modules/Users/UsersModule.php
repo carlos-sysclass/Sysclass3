8cb95d442359668a78d934e295f6723830a64a3c
Carlos Oliveira <carlos@sysclass.com>
2017-08-22 18:53:50 -0500

    enrollment@lucent.institute

--- modules/Users/UsersModule.php
+++ modules/Users/UsersModule.php
@@ -389,7 +389,7 @@ class UsersModule extends \SysclassModule implements \ILinkable, \IBlockProvider
 
 				$status = $this->mail->send(
 					"enrollment@lucent.institute",
-					"A new enrollment has been made in lucent website.",
+					"A new enrollment has been made at the Lucent website.",
 					$template,
 					true,
 					array(
