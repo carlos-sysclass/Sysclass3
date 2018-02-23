dad3eba2440ece6094ca2af1b02bbe71762f826f
Paulo Nhaia <paulo.nhaia@gmail.com>
2018-02-22 22:50:46 -0300

    Ajust rules enroll.

--- controller/ApiController.php
+++ controller/ApiController.php
@@ -241,10 +241,10 @@ class ApiController extends \AbstractSysclassController {
 			if (is_null($postdata)) {
 				$messages[] = $this->invalidRequestError(self::INVALID_DATA, "warning");
 				$error = true;
-			}else if( validAge($postdata['birthday']) ){
-				$messages[] = $this->invalidRequestError(self::INVALID_AGE, "warning");
-				$error = true;
-			}else if( $postdata['secondary_school'] == 'Not Completed' && ($postdata['courses'] == 9 || $postdata['courses'] == 10) ){	
+			//}else if( validAge($postdata['birthday']) ){
+			//	$messages[] = $this->invalidRequestError(self::INVALID_AGE, "warning");
+			//	$error = true;
+			}else if( $postdata['secondary_school'] == 'Not Completed' && ($postdata['courses'] == 9 || $postdata['courses'] == 10) ){
 				$messages[] = $this->invalidRequestError(self::INVALID_ENROLL_ASSOCIATE, "warning");
 				$error = true;
 			}else if( $postdata['higher_school'] == 'Not Completed' && $postdata['courses'] == 10 ){
